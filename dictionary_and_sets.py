b3365585ba65f0c2a1a1f0000c06d979ff0394ac
Forhad <md.foredul.islam.forhad@gmail.com>
2026-08-07 17:54:58 +0600

    7th August

--- dictionary_and_sets.py
+++ dictionary_and_sets.py
@@ -20,6 +20,10 @@ Practice methods
 •	pop()
 """
 
+from email.policy import default
+
+from networkx import symmetric_difference
+from numpy import sort
 from pygments import highlight
 
 
@@ -123,6 +127,207 @@ print(min(prices.items(), key=lambda item: item[1]))
 print(sum(prices.values()))
 
 # %%
+# Part 3: Dictionary Comprehensions
 
+"""
+Exercise 6
+Given
+numbers = [1,2,3,4,5]
+Create
+{
+    1:1,
+    2:4,
+    3:9,
+    4:16,
+    5:25
+}
+"""
+numbers = [1, 2, 3, 4, 5]
+
+sqr_numbers = {n:n**2 for n in numbers}
+
+print(sqr_numbers)
+
+# %%
+# Exercise 7
+"""
+Given
+students = {
+    "Alice":88,
+    "Bob":61,
+    "Charlie":95,
+    "David":58
+}
+Create a new dictionary containing only students with marks ≥70.
+"""
+students = {"Alice": 88, "Bob": 61, "Charlie": 95, "David": 58}
+
+filtered_students = {
+    student: marks for student, marks in filter(lambda x: x[1] >= 70, students.items())
+}
+
+print(filtered_students)
+
+# %%
+"""
+Convert
+{
+    "a":1,
+    "b":2,
+    "c":3
+}
+into
+{
+    1:"a",
+    2:"b",
+    3:"c"
+}
+"""
+
+primary_dictionary = {"a": 1, "b": 2, "c": 3}
+
+converted_dictionary = {value: key for key, value in  primary_dictionary.items()}
+
+print(converted_dictionary)
+
+# %%
+# Part 4: Sets
+"""
+Exercise 9
+Create
+A = {1,2,3,4}
+B = {3,4,5,6}
+Find
+•	Union
+•	Intersection
+•	Difference
+•	Symmetric difference
+"""
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+
+C:set[int] = A | B
+D:set[int] = A & B
+E:set[int] = A - B
+F:set[int] = A ^ B
+print(f"Union: {C}")
+print(f"Intersection: {D}")
+print(f"Difference: {E}")
+print(f"Symmetric difference: {F}")
+
+
+# %%
+"""
+Exercise 10
+Using methods
+A.union(B)
+A.intersection(B)
+A.difference(B)
+A.symmetric_difference(B)
+Compare them with the operators above.
+"""
+
+union_set = A.union(B)
+intersection_set = A.intersection(B)
+difference_set = A.difference(B)
+symmetric_difference_set = A.symmetric_difference(B)
+
+print(union_set)
+print(intersection_set)
+print(difference_set)
+print(symmetric_difference_set)
+
+# %%
+# Exercise 11
+"""
+Practice
+add()
+remove()
+discard()
+pop()
+clear()
+Questions
+•	Add 10
+•	Remove 5
+•	Try removing 100
+•	What's the difference between remove() and discard()?
+"""
+my_set = {5, 9, 6, 2}
+my_set.add(10)
+my_set.remove(5)
+my_set.discard(100)
+my_set.pop()
+print(my_set)
+"""The difference between remove() and discard() is raising KeyError. using remove() returns a KeyError if not found and discard() doesn't raise any error"""
+# %%
+"""
+Part 5: Set Comprehensions
+Exercise 12
+Given
+numbers = [1,2,2,3,3,4,5,5]
+Create a set of squares.
+Expected
+{1,4,9,16,25}
+"""
+numbers = [1, 2, 2, 3, 3, 4, 5, 5]
+print(set(number ** 2 for number in numbers))
+
+# %%
+"""
+Exercise 13
+Given
+words = ["Apple","Banana","APPLE","banana"]
+Create a set containing unique lowercase words.
+Expected
+{"apple","banana"}
+"""
+words = ["Apple", "Banana", "APPLE", "banana"]
+words_sorted = sorted(words)
+print(words_sorted)
+print(set(word.lower() for word in words))
+print({word.lower() for word in words})
+# %%
+# Part 6: Real-World Problems
+# Exercise 14: Website Visitors
+
+"""
+Find
+•	New visitors
+•	Returning visitors
+•	Visitors who came only yesterday
+"""
+today = {"Alice", "Bob", "David", "John"}
+yesterday = {"Bob", "David", "Emma", "Frank"}
+
+new_visitors = yesterday ^ today
+returning_visitors = today & yesterday
+yesterday_visitors = yesterday - today
+print(f"New Visitors: {new_visitors}")
+print(f"Returning Visitors: {returning_visitors}")
+print(f"Yesterday Visitors: {yesterday_visitors}")
+
+
+# %%
+# Exercise 15: Shopping Cart
+
+"""
+Calculate
+•	Total bill
+•	Most expensive purchased item
+•	Average item price
+"""
+
+cart = {"Apple": 4, "Banana": 2, "Milk": 1}
+
+prices = {"Apple": 30, "Banana": 15, "Milk": 80}
+
+print(sum(cart * price for cart, price in zip(cart.values(), prices.values())))
+print(max(zip(cart.items(), prices.items()), key=lambda x: x[1]))
+
+
+
+# %%
+for cart_item, item_price in zip(cart.items(), prices.items()):
+    print(item_price[1] * cart_item[1])
 
 # %%
