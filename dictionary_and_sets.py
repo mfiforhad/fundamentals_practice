e9026d3ee6f4c9a4b7fc060aaae2ff325856a73c
Forhad <md.foredul.islam.forhad@gmail.com>
2026-08-08 23:50:43 +0600

    all problems are solved

--- dictionary_and_sets.py
+++ dictionary_and_sets.py
@@ -23,6 +23,8 @@ Practice methods
 grades: dict = {"Alice": 88, "Bob": 72, "Charlie": 95}
 
 print(grades.get("Bob"))
+grades.update(David=88)
+print(grades)
 grades["Alice"] = 91
 grades.pop("Charlie", "Not Found")
 print(grades.keys())
@@ -95,7 +97,7 @@ Questions
 4.	Empty the original dictionary.
 
 """
-# %%
+
 inventory: dict[str, int] = {"apple": 30, "banana": 12, "orange": 18}
 
 inventory.update(grape=20, banana=25)
@@ -104,9 +106,12 @@ inventory.setdefault("mango", 38)
 
 inventory2 = inventory.copy()
 
+inventory.clear()
+print(inventory)
+
 print(inventory2)
 
-# %%
+
 """
 Exercise 5
 Find
@@ -119,7 +124,7 @@ print(max(prices.items(), key=lambda item: item[1]))
 print(min(prices.items(), key=lambda item: item[1]))
 print(sum(prices.values()))
 
-# %%
+
 # Part 3: Dictionary Comprehensions
 
 """
@@ -141,7 +146,7 @@ sqr_numbers = {n: n**2 for n in numbers}
 
 print(sqr_numbers)
 
-# %%
+
 # Exercise 7
 """
 Given
@@ -161,7 +166,7 @@ filtered_students = {
 
 print(filtered_students)
 
-# %%
+
 """
 Convert
 {
@@ -183,7 +188,7 @@ converted_dictionary = {value: key for key, value in primary_dictionary.items()}
 
 print(converted_dictionary)
 
-# %%
+
 # Part 4: Sets
 """
 Exercise 9
@@ -209,7 +214,6 @@ print(f"Difference: {E}")
 print(f"Symmetric difference: {F}")
 
 
-# %%
 """
 Exercise 10
 Using methods
@@ -230,7 +234,7 @@ print(intersection_set)
 print(difference_set)
 print(symmetric_difference_set)
 
-# %%
+
 # Exercise 11
 """
 Practice
@@ -252,7 +256,7 @@ my_set.discard(100)
 my_set.pop()
 print(my_set)
 """The difference between remove() and discard() is raising KeyError. using remove() returns a KeyError if not found and discard() doesn't raise any error"""
-# %%
+
 """
 Part 5: Set Comprehensions
 Exercise 12
@@ -265,7 +269,7 @@ Expected
 numbers = [1, 2, 2, 3, 3, 4, 5, 5]
 print(set(number**2 for number in numbers))
 
-# %%
+
 """
 Exercise 13
 Given
@@ -279,7 +283,7 @@ words_sorted = sorted(words)
 print(words_sorted)
 print(set(word.lower() for word in words))
 print({word.lower() for word in words})
-# %%
+
 # Part 6: Real-World Problems
 # Exercise 14: Website Visitors
 
@@ -289,10 +293,11 @@ Find
 •	Returning visitors
 •	Visitors who came only yesterday
 """
+# %%
 today = {"Alice", "Bob", "David", "John"}
 yesterday = {"Bob", "David", "Emma", "Frank"}
 
-new_visitors = yesterday ^ today
+new_visitors = today - yesterday
 returning_visitors = today & yesterday
 yesterday_visitors = yesterday - today
 print(f"New Visitors: {new_visitors}")
@@ -300,7 +305,6 @@ print(f"Returning Visitors: {returning_visitors}")
 print(f"Yesterday Visitors: {yesterday_visitors}")
 
 
-# %%
 # Exercise 15: Shopping Cart
 """
 Calculate
@@ -313,9 +317,9 @@ cart = {"Apple": 4, "Banana": 2, "Milk": 1}
 
 prices = {"Apple": 30, "Banana": 15, "Milk": 80}
 
-print(
-    f"Total bill: {sum(cart * price for cart, price in zip(cart.values(), prices.values()))}"
-)
+# print(f"Total bill: {sum(cart * price for cart, price in zip(cart.values(), prices.values(), strict=True))}")
+
+print(sum(value * prices[key] for key, value in cart.items()))
 
 print(
     f"Most expensive purchased item: {max(cart, key=lambda item: cart[item] * prices[item])}"
@@ -325,7 +329,7 @@ print(f"Average item price: {round(sum(prices.values()) / len(prices.values()), 
 
 print(f"Expensive Product: {max(prices, key= lambda item: prices[item])}")
 
-# %%
+
 # Exercise 16: Inventory Merge
 """
 Warehouse A
@@ -352,5 +356,100 @@ Expected
 Warehouse_A: dict[str, int] = {"Pen": 40, "Book": 20, "Pencil": 60}
 Warehouse_B: dict[str, int] = {"Book": 15, "Pen": 30, "Eraser": 50}
 
+merged_warehouse: dict[str, int] = {**Warehouse_A}
 
-# %%
+for key, value in Warehouse_B.items():
+    merged_warehouse[key] = value + merged_warehouse.get(key, 0)
+
+print(merged_warehouse)
+
+
+# Exercise 17: Employee Department Lookup
+
+"""
+Create
+{
+    "Sales":[101,105],
+    "IT":[102,104],
+    "HR":[103]
+}
+"""
+
+employees = {101: "Sales", 102: "IT", 103: "HR", 104: "IT", 105: "Sales"}
+
+department = {}
+
+for key, value in employees.items():
+    department.setdefault(value, []).append(key)
+
+print(department)
+
+
+# Exercise 18: Common Skills
+"""
+Find
+•	Common skills
+•	Skills only Alice has
+•	Skills only Bob has
+•	All unique skills
+"""
+
+alice = {"Python", "SQL", "Excel", "Power BI"}
+
+bob = {"Python", "Java", "SQL", "AWS"}
+
+common_skills = alice & bob
+print(common_skills)
+alice_skills = alice - bob
+print(alice_skills)
+bob_skill = bob - alice
+print(bob_skill)
+unique_skill = alice ^ bob
+print(unique_skill)
+
+
+# Exercise 19: Log Analysis
+"""
+Create
+{
+    "INFO":3,
+    "ERROR":3,
+    "WARNING":1
+}
+Then determine the most frequent log level.
+"""
+
+
+logs = ["INFO", "ERROR", "INFO", "WARNING", "ERROR", "ERROR", "INFO"]
+
+logs_analysis = {}
+
+for k in logs:
+    logs_analysis[k] = logs.count(k)  # logs_analysis.get(k, 0) + 1
+
+most_frequent_log = max(logs_analysis.values())
+
+print(most_frequent_log)
+
+
+# Exercise 20: Movie Recommendations
+"""
+•	Common movies
+•	Movies to recommend to User A
+•	Movies to recommend to User B
+"""
+
+
+user_a = {"Inception", "Interstellar", "Avatar", "Titanic"}
+
+user_b = {"Titanic", "Avatar", "The Matrix", "Dune"}
+
+common_movies = user_a & user_b
+
+recommended_user_a = user_b - user_a
+
+recommended_user_b = user_a - user_b
+
+print(f"Common movies: {common_movies}")
+print(f"Movies to recommend to User A: {recommended_user_a}")
+print(f"Movies to recommend to User B: {recommended_user_b}")
